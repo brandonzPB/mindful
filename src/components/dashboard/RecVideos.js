7ad9164017ad330439519a2c72b75c3200fbcae7
Brandon Zirulnikoff <zirbrandonweb@gmail.com>
2021-02-28 11:53:28 -0800

    Added video modal, styling

--- src/components/dashboard/RecVideos.js
+++ src/components/dashboard/RecVideos.js
@@ -1,10 +1,12 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { UserContext } from '../../contexts/UserContext';
 import './recVideos.css';
 
 const RecVideos = () => {
   const { setDest, link } = useContext(UserContext);
+
+  const [currentVideo, setCurrentVideo] = useState({ id: 0 });
 
   // if (!user.accessToken) {
   //   return (
@@ -13,6 +15,30 @@ const RecVideos = () => {
   //     </Route>
   //   )
   // }
+
+  const nextVideo = () => {
+    currentVideo.id < 5
+      ? setCurrentVideo({
+        ...currentVideo,
+        id: currentVideo.id + 1
+      })
+      : setCurrentVideo({
+        ...currentVideo,
+        id: 0
+      });
+  }
+
+  const previousVideo = () => {
+    currentVideo.id > 0
+      ? setCurrentVideo({
+        ...currentVideo,
+        id: currentVideo.id - 1
+      })
+      : setCurrentVideo({
+        ...currentVideo,
+        id: 5
+      });
+  }
 
   if (link.dest === 'dashboard') {
     return (
@@ -39,67 +65,76 @@ const RecVideos = () => {
         <button className="dest-btns entry-route-btn" onClick={() => setDest('entry')}>Boost Mindfulness</button>
       </div>
 
-      <div className="video-container">
-        <span className="video-text">5 Minute Guided Breathing Meditation by Diana Winston</span>
-        <iframe title="breathing-meditation"
-          width="560" height="315" src="https://www.youtube.com/embed/YFSc7Ck0Ao0" 
-          frameBorder="0" allowFullScreen className="iframe"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
-        </iframe>
-      </div>
+      <div id="video-modal">
+        <div id="diana-container">
+          <div className="video-container" style={{ display: currentVideo.id === 0 ? 'block' : 'none' }} >
+            <span className="video-text">5 Minute Guided Breathing Meditation by Diana Winston</span>
+            <iframe title="breathing-meditation"
+              width="560" height="315" src="https://www.youtube.com/embed/YFSc7Ck0Ao0" 
+              frameBorder="0" allowFullScreen className="iframe"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
+            </iframe>
+          </div>
 
-      <div id="extra-video-container">
-        <a href="https://dianawinston.com/meditations" target="_blank" rel="noreferrer">
-          <span id="extra-video-text">Click here for more guided meditations by Diana Winston</span>
-        </a>
-      </div>
-      
-      <div className="videos">
-        <div className="video-container">
-          <span className="video-text">10 Minute Guided Meditation for Beginners</span>
-          <iframe title="beginner-meditation"
-            width="560" height="315" src="https://www.youtube.com/embed/Evgx9yX2Vw8" 
-            frameBorder="0" allowFullScreen className="iframe"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
-          </iframe>
-        </div>
-
-        <div className="video-container">
-          <span className="video-text">15 Minute Guided Meditation by Tibetan Buddhist Meditation Master</span>
-          <iframe title="buddhist-meditation"
-            width="560" height="315" src="https://www.youtube.com/embed/5GSeWdjyr1c" 
-            frameBorder="0" allowFullScreen className="iframe"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
-          </iframe>
+          <div id="extra-video-container">
+            <a href="https://dianawinston.com/meditations" target="_blank" rel="noreferrer">
+              <span id="extra-video-text">Click here for more guided meditations by Diana Winston</span>
+            </a>
+          </div>
         </div>
         
-        <div className="video-container">
-          <span className="video-text">15 Minute Guided Meditation by Alan Watts</span>
-          <iframe title="watts-meditation"
-            width="560" height="315" src="https://www.youtube.com/embed/jPpUNAFHgxM" 
-            frameBorder="0" allowFullScreen className="iframe"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
-          </iframe>
-        </div>
-        
+        <div className="videos">
+          <div className="video-container" style={{ display: currentVideo.id === 1 ? 'block' : 'none' }} >
+            <span className="video-text">10 Minute Guided Meditation for Beginners</span>
+            <iframe title="beginner-meditation"
+              width="560" height="315" src="https://www.youtube.com/embed/Evgx9yX2Vw8" 
+              frameBorder="0" allowFullScreen className="iframe"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
+            </iframe>
+          </div>
 
-        <div className="video-container">
-          <span className="video-text">10 Minute Guided Meditation for Anxiety Relief</span>
-          <iframe title="anxiety-meditation"
-            width="560" height="315" src="https://www.youtube.com/embed/O-6f5wQXSu8" 
-            frameBorder="0" allowFullScreen className="iframe"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
-          </iframe>
-        </div>
-        
+          <div className="video-container" style={{ display: currentVideo.id === 2 ? 'block' : 'none' }} >
+            <span className="video-text">15 Minute Guided Meditation by Tibetan Buddhist Meditation Master</span>
+            <iframe title="buddhist-meditation"
+              width="560" height="315" src="https://www.youtube.com/embed/5GSeWdjyr1c" 
+              frameBorder="0" allowFullScreen className="iframe"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
+            </iframe>
+          </div>
+          
+          <div className="video-container" style={{ display: currentVideo.id === 3 ? 'block' : 'none' }} >
+            <span className="video-text">15 Minute Guided Meditation by Alan Watts</span>
+            <iframe title="watts-meditation"
+              width="560" height="315" src="https://www.youtube.com/embed/jPpUNAFHgxM" 
+              frameBorder="0" allowFullScreen className="iframe"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
+            </iframe>
+          </div>
+          
 
-        <div className="video-container">
-          <span className="video-text">11 Minute Guided Meditation for a New You</span>
-          <iframe title="new-meditation"
-            width="560" height="315" src="https://www.youtube.com/embed/lhtAj91Lpgw" 
-            frameBorder="0" allowFullScreen className="iframe"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
-          </iframe>
+          <div className="video-container" style={{ display: currentVideo.id === 4 ? 'block' : 'none' }} >
+            <span className="video-text">10 Minute Guided Meditation for Anxiety Relief</span>
+            <iframe title="anxiety-meditation"
+              width="560" height="315" src="https://www.youtube.com/embed/O-6f5wQXSu8" 
+              frameBorder="0" allowFullScreen className="iframe"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
+            </iframe>
+          </div>
+          
+
+          <div className="video-container" style={{ display: currentVideo.id === 5 ? 'block' : 'none' }} >
+            <span className="video-text">11 Minute Guided Meditation for a New You</span>
+            <iframe title="new-meditation"
+              width="560" height="315" src="https://www.youtube.com/embed/lhtAj91Lpgw" 
+              frameBorder="0" allowFullScreen className="iframe"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture">
+            </iframe>
+          </div>
+        </div>
+
+        <div id="btns-container">
+          <button id="prev-btn" onClick={previousVideo}>Previous</button>
+          <button id="next-btn" onClick={nextVideo}>Next</button>
         </div>
       </div>
     </div>
